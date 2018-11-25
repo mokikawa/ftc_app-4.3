d096bc8083990b0f87554776f3fc3084088d4315
mokikawa <mark.okikawa@gmail.com>
2018-11-25 10:04:54 -1000

    Updated Class Names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/B2CDcMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/B2CDcMotor.java
@@ -2,18 +2,23 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class B2CDcMotor {
 
     //Declare OpMode Members
-    public DcMotor leftDrive;
-    public DcMotor rightDrive;
-    public DcMotor armDrive;
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+    public DcMotor armDrive = null;
 
     //Initialize hardware variables
     leftDrive = HardwareMap.get (DcMotor.class, "left_drive");
     rightDrive = HardwareMap.get (DcMotor.class, "right_drive");
     armDrive = HardwareMap.get (DcMotor.class,"arm_drive");
+
+    public B2CDcMotor (Telemetry telemetry) {
+        this.Telemetry = telemetry;
+    }
 
     //Set motor directions
     leftDrive.setDirection (DcMotor.Direction.FORWARD);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/B2CRobot.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
-
-public class B2CRobot {
-
-    public
-}
