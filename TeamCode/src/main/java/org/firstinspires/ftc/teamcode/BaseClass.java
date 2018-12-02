047631745799c49e9406a9b423638a1ce0cb30f2
mokikawa <31202380+mokikawa@users.noreply.github.com>
2018-12-02 10:52:11 -1000

    Practice Code from Youtube with comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseClass.java
@@ -1,24 +1,40 @@
+/*
+Beginning Comments
+Class Description
+Mark Okikawa 2018
+*/
+
+// Declare package
 package org.firstinspires.ftc.teamcode;
 
+// Import statements if any
+
+// File Class
 public class  BaseClass {
 	
+	
+// Declare datatypes
 	public int myNum;
 	public string myName;
 	
+// Create constructor
 	public BaseClass() {
-		myNum = 5;
-		myName = "Mark";
+		myNum = 5;			// Set initial myNum to equal 5
+		myName = "Mark";	// Set initial myName to equal "Mark"
 	}
-	
-	public BaseClass (in newNum) {
+
+// 
+	public BaseClass (int newNum) {
 		myNum = newNum;
 		myName = "Mark";
 	}
-	
+
+// Create changeNum method
 	public void changeNum(int changeBy) {
 		myNum += changeBy;
 	}
 	
+// Create changeName method
 	public void changeName(String newName) {
 		myName = newName;
 	}
